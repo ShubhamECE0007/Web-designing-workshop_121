ab6f85fc17a0500f907e327f03e8595a72fbc1e5
SHUBHAM-SINGH-ECE01 <shubham.parashar2007@gmail.com>
2026-03-11 12:21:17 +0530

    initialcommit

--- ece2.js/add.js
+++ ece2.js/add.js
@@ -1,5 +1,8 @@
- console.log("hi javascript");
-        alert("welcome to javacript")
-        document.body.style.backgroundcolor = "lightblue";
+console.log("hi javascript i m shreyansh");
+        alert("welcome to javascript");
+        document.body.style.backgroundcolor=" lightblue";
+        
 
-
+        GamepadButton.addEventListener('click', function(){
+            alert('button clicked!')
+        });
