e8403b1495ba72d937e85a7a39fb581aa7154347
SHUBHAM-SINGH-ECE01 <shubham.parashar2007@gmail.com>
2026-04-15 11:39:10 +0530

    experiment no. 6

--- ece2.js/add.js
+++ ece2.js/add.js
@@ -1,8 +1,39 @@
-console.log("hi javascript i m shreyansh");
-        alert("welcome to javascript");
-        document.body.style.backgroundcolor=" lightblue";
-        
+let heading = document.getElementById("heading")
+    let input = document.getElementById("inputText")
+    let para = document.getElementById("Para")
 
-        GamepadButton.addEventListener('click', function(){
-            alert('button clicked!')
-        });
+// change heading
+    document.getElementById("changetextBtn").onclick = function(){ 
+        heading.innerText=input.value;
+    };
+    // change event
+    input.onchnage  = function(){
+        console.log("Input changed : ", input .value);
+
+    };
+    document.getElementById("bgColorBtn").addEventListener("click", function() {
+        documnet.body.style.backgroundColor="lightblue";
+    });
+      document.getElementById("fontSizeBtn").addEventListener("click",function(){
+        heading.style.fontSize="40px";
+      });
+    //   show/hide Paragraph
+
+    let isVisible = "true";
+    documnet.getElementById("toggleParBtn").oneclick=function(){
+        if(isVisible){
+            para.style.display = "none";
+            isVisible = "false"
+        }
+        else{
+            para.style.display = "block";
+            isVisible = true;
+        }
+    }
+    document.getElementById("resetBtn").onclick = function(){
+        location.reload();
+    };
+     heading.onmouseover = function(){
+        heading.style.color="blue";
+     }
+    ;
